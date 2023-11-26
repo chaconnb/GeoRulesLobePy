dbb3d3025e787c55f3ed8992a1b31f7bcb88edad
Daniel <45088478+dwillhelm@users.noreply.github.com>
2023-11-26 11:23:50 -0600

    REF: `Lob_map()` function.  (#4)
    * refactor lobe geometry
    
    * fix missing import
    
    * mv function to new module; rm old file
    
    * update w/ new bathymetry class
    
    * init bathymetry module
    
    * minor cleanup
    
    * Add "scaled" dimensions to `LobeGeometry`; update logic.
    
    Add new class attributes `scaled_width` and `scaled_length`.
    
    * extract a few subfunctions; CLN
    
    * CLN: more readable

--- georules/S_ProbMap.py
+++ georules/S_ProbMap.py
@@ -3,15 +3,15 @@
 
 @author: Nataly Chacon-Buitrago
 
-lobe_map - Function generating the bathymetric map  as the nlb lobes are deposited
+lobe_map - Function generating the bathymetric map  as the num_of_lobes lobes are deposited
 inputs: 
     nx = number of cells
     ny = number of cells
     cell_size = size of each cell in m
-    wmax = maximum lobe width int
+    width = maximum lobe width int
     length = maximum lobe length int
     tmax = maximum thickness int
-    nlb = number of lobes to deposit
+    num_of_lobes = number of lobes to deposit
     power = compensation power (See Jo. 2020)
     tm = transition matrix for markov chain
     startstate = state (the state is the quadrant- there are 4 quadrants the user can determine the angles betwwen each quadrant)
@@ -25,8 +25,6 @@ output:
 
 """
 import numpy as np
-from S_Lobegeom import drop_geometry
-from S_LobeElement import LobeDepos
 from S_Stacking import stacking
 from S_Markov import stack_forecast
 from S_RotationAngle import rot_angle
@@ -37,36 +35,54 @@ from S_RotationAngle import rot_angle
 #nx = 400 
 #ny = 400 
 #cell_size = 100 
-#wmax = 15000 
+#width = 15000 
 #lenght = 30000 
 #tmax = 2 
-#nlb = 10  
+#num_of_lobes = 10  
 #power = 5
 
+# import from refactors
+from lobes import LobeGeometry, lobe_deposition
+from bathymetry import BathymetryLayers
 
-def Lobe_map(nx, ny,cell_size,wmax,lenght,tmax,nlb,power, tm, startstate,quadrant_angles,source,a1,a2,cellsize_z,n_mud):
+def normalize_probability(prob_s) -> np.ndarray:
+    """Normalize the ##WHAT## Probability.""" 
+    # NOTE: needs better docstring
+    prob_sum = np.sum(prob_s) #prob_s has to be positive
+    norm_prob_s = prob_s/prob_sum
+    return norm_prob_s
 
-    # Lobe grid dimensions #wmax #lenght #tmax
-    x = [i for i in range(0,lenght, cell_size)]
-    y_interval = cell_size
+def get_norm_elevation(bathymetry:BathymetryLayers, idx:int=None) -> np.ndarray:
+    """Create probability map of the elevations of the mud.""" 
+    idx = -1 if idx is None else idx
+    inv_bath = 1 - bathymetry.layers[idx]
+    norm_elevation = (inv_bath - inv_bath.min())/(inv_bath.max() - inv_bath.min())
+    return norm_elevation
 
+def Lobe_map(
+        nx,
+        ny,
+        cell_size,
+        width,
+        lenght,
+        tmax,
+        num_of_lobes,
+        power,
+        tm,
+        startstate,
+        quadrant_angles,
+        source,
+        cellsize_z,
+        n_mud
+    ):
+    """Lobe Map"""
     # Calculate lobe array with drop geometry
-    lobe_array = drop_geometry(wmax,lenght,tmax,x,y_interval,a1,a2)
+    lobe_geometry = LobeGeometry(width=width,length=lenght, tmax=tmax, cell_size=cell_size)
 
     # Set initial bathymetry
-    # zero surface
-    Bathymetry_ini = np.zeros((nx,ny))
+    bathymetry = BathymetryLayers(nx, ny)
 
-
-    # Surface Modeling
-    Bathymetry_ = Bathymetry_ini
-    Bathymetry_steps = []
-    Bathymetry_steps.append(Bathymetry_.copy()) # the first layer is a zero bathymetry layer
     n=0 
-
-    # scale lenght and wmax to cell size
-    lenght_new = lenght/cell_size 
-    wmax_new = wmax/cell_size
 
     #Create list with centroids
     ls = []
@@ -75,7 +91,7 @@ def Lobe_map(nx, ny,cell_size,wmax,lenght,tmax,nlb,power, tm, startstate,quadran
     ps = []
 
     # Use markov-chains to find stacking patterns
-    stack_list = stack_forecast(startstate,nlb,tm) #function inputs start state ="Q1"
+    stack_list = stack_forecast(startstate, num_of_lobes, tm) # function inputs start state ="Q1"
     print(stack_list)
     #source= channel
     
@@ -85,21 +101,17 @@ def Lobe_map(nx, ny,cell_size,wmax,lenght,tmax,nlb,power, tm, startstate,quadran
     row_corner_list = []
     
 
-    while n < nlb: 
-
-
+    while n < num_of_lobes: 
         if n == 0:
-            
             #Lobe Placement
             # Select Source Location - centroid (No modification)
-            elevation_s = (Bathymetry_ - np.min(Bathymetry_))/(np.max(Bathymetry_)+0.0001)+0.0001
-            prob_s = (1/np.transpose(elevation_s))**power ## compensational power  ###Sera necesario tenerlo que trasponer???\
-            prob_s[:,int(lenght_new):int(prob_s.shape[1])] = 0 
+            elevation_s = bathymetry.get_elevation(n)
+            prob_s = (1/np.transpose(elevation_s))**power ## compensational power  ### Sera necesario tenerlo que trasponer???\
+            prob_s[:,int(lobe_geometry.scaled_length):int(prob_s.shape[1])] = 0 
             ps.append(prob_s)
                 
-            #normalize prob_s
-            prob_sum = np.sum(prob_s) #prob_s has to be positive
-            norm_prob_s = prob_s/prob_sum
+            norm_prob_s = normalize_probability(prob_s)
+
             # Use numpy.random.choice to select a flattened index - the centroid- with the specified weights
             index = np.random.choice(elevation_s.size, p=norm_prob_s.flatten())
             # Convert the flattened index to a row and column index
@@ -111,8 +123,21 @@ def Lobe_map(nx, ny,cell_size,wmax,lenght,tmax,nlb,power, tm, startstate,quadran
             rotation_angle = rot_angle(Location_,source)
             angle_list.append(rotation_angle) 
             
-            Bathymetry_,thick_updated,col_corn,row_corn = LobeDepos(Location_, lenght_new, wmax_new, lobe_array, Bathymetry_ini.copy()
-                                                                              ,Bathymetry_,nx,ny,rotation_angle)
+            res = lobe_deposition(
+                Location_,
+                lobe_geometry.scaled_length,
+                lobe_geometry.scaled_width,
+                lobe_geometry.lobe_thickness,
+                rotation_angle,
+                bathymetry,
+            )
+            Bathymetry_, thick_updated, col_corn, row_corn = res
+            
+            bathymetry.add_layer(Bathymetry_)
+            print("plot layer")
+            fig, ax = bathymetry._plot_layer(f'layer-{n}', idx=n, save=False)
+            fig.show()
+            
             column_corner_list.append(col_corn)
             row_corner_list.append(row_corn)
         
@@ -124,13 +149,12 @@ def Lobe_map(nx, ny,cell_size,wmax,lenght,tmax,nlb,power, tm, startstate,quadran
            if current_state == "NMA":
                
                # Select Source Location - centroid (No modification)
-               elevation_s = (Bathymetry_ - np.min(Bathymetry_))/(np.max(Bathymetry_)+0.0001)+0.0001
+               elevation_s = bathymetry.get_elevation(n)
                prob_s = (1/np.transpose(elevation_s))**power ## compensational power  ###Sera necesario tenerlo que trasponer???\
                ps.append(prob_s)
                    
-               #normalize prob_s
-               prob_sum = np.sum(prob_s) #prob_s has to be positive
-               norm_prob_s = prob_s/prob_sum
+               norm_prob_s = normalize_probability(prob_s)
+
                # Use numpy.random.choice to select a flattened index - the centroid- with the specified weights
                index = np.random.choice(elevation_s.size, p=norm_prob_s.flatten())
                # Convert the flattened index to a row and column index
@@ -143,20 +167,27 @@ def Lobe_map(nx, ny,cell_size,wmax,lenght,tmax,nlb,power, tm, startstate,quadran
                rotation_angle = rot_angle(Location_,source) 
                angle_list.append(rotation_angle)
                
-               Bathymetry_,thick_updated,col_corn,row_corn = LobeDepos(Location_, lenght_new, wmax_new, lobe_array, Bathymetry_ini.copy()
-                                                                                  ,Bathymetry_,nx,ny,rotation_angle) 
+               res = lobe_deposition(
+                   Location_,
+                   lobe_geometry.scaled_length,
+                   lobe_geometry.scaled_width,
+                   lobe_geometry.lobe_thickness,
+                   rotation_angle,
+                   bathymetry,
+                ) 
+               Bathymetry_, thick_updated, col_corn, row_corn = res
+               bathymetry.add_layer(Bathymetry_)
+               
+               print("plot layer")
+               fig, ax = bathymetry._plot_layer(f'layer-{n}', idx=n, save=False)
+               fig.show()
+
                column_corner_list.append(col_corn)
                row_corner_list.append(row_corn)
                
            elif current_state == "HF":
                
-               #Bathymetry_ = np.transpose(Bathymetry_)
-               Bathymetry_inverse = 1 - Bathymetry_ 
-               min_value = np.min(Bathymetry_inverse)
-               max_value = np.max(Bathymetry_inverse)
-
-               #create probability map of the elevations of the mud
-               norm_elevation = (Bathymetry_inverse-min_value)/(max_value-min_value)
+               norm_elevation = get_norm_elevation(bathymetry)
 
                #create an array with mud thickness
                layer_mud_thick = cellsize_z * n_mud
@@ -168,32 +199,64 @@ def Lobe_map(nx, ny,cell_size,wmax,lenght,tmax,nlb,power, tm, startstate,quadran
                Bathymetry_ = Bathymetry_ + mud_thickness
                ls.append([]),ps.append([]),column_corner_list.append([]),row_corner_list.append([]),angle_list.append([])
                
+               # update bathymetry
+               bathymetry.add_layer(Bathymetry_)
+               print("plot layer")
+               fig, ax = bathymetry._plot_layer(f'layer-{n}', idx=n, save=False)
+               fig.show()
         
            else:
                angle1 = quadrant_angles[current_state][0]
                angle2 = quadrant_angles[current_state][1]
-           
                
                # Stacking 
-               Location_, prob_s, prob_bsm = stacking(ls, n, wmax_new, 0.2, nx, ny, Bathymetry_,power,angle1,angle2) # 0.2 debe ser cambiados por valores de funciones
+               Location_, prob_s, prob_bsm = stacking(ls, n, lobe_geometry.scaled_width, 0.2, nx, ny, bathymetry.layers[-1], power,angle1, angle2) # 0.2 debe ser cambiados por valores de funciones
                ls.append(Location_),ps.append(prob_bsm)
            
                #Find rotation angle
                rotation_angle = rot_angle(Location_,source)
                angle_list.append(rotation_angle)
            
-               Bathymetry_,thick_updated,col_corn,row_corn = LobeDepos(Location_, lenght_new, wmax_new, lobe_array, Bathymetry_ini.copy()
-                                                                             ,Bathymetry_,nx,ny,rotation_angle)
+               res = lobe_deposition(
+                   Location_,
+                   lobe_geometry.scaled_length,
+                   lobe_geometry.scaled_width,
+                   lobe_geometry.lobe_thickness,
+                   rotation_angle, 
+                   bathymetry,
+                )
+               Bathymetry_, thick_updated, col_corn, row_corn = res
                
                column_corner_list.append(col_corn)
                row_corner_list.append(row_corn)
-            
-            
-        Bathymetry_steps.append(Bathymetry_.copy())
+
+               # update bathymetry
+               bathymetry.add_layer(Bathymetry_)
+               print("plot layer")
+               fig, ax = bathymetry._plot_layer(f'layer-{n}', idx=n, save=False)
+               fig.show()
+
+        # NOTE: `Bathymetry_steps` was being used as a "Global Variable" :(
+        Bathymetry_steps = bathymetry.layers.copy()
         
         n = n+1 
         print(n) #track progress
         
-    return(Bathymetry_steps,ls,ps,stack_list,angle_list,column_corner_list,row_corner_list,lobe_array) 
+        # prepare output
+        output = (
+            Bathymetry_steps,
+            ls,
+            ps,
+            stack_list,
+            angle_list,
+            column_corner_list,
+            row_corner_list,
+            lobe_geometry.lobe_thickness
+        )
+    return output 
+
+
+
+
 
     
