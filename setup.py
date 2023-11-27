d91059e46c8611ecba9b4cee9e09b95f76b9a911
dwillhelm <dwillhelm@enthought.com>
2023-11-27 11:00:11 -0600

    add `setup.py`

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "georules"
+PACKAGE_DIRS = [PACKAGE_NAME]
+VERSION = None
+RUNTIME_DEPS = None
+
+
+def main():
+    """Run setup."""
+    print("\nRunning `setup.py`...")
+    setup(
+        name=PACKAGE_NAME,
+        version=VERSION,
+        install_requires=RUNTIME_DEPS,
+        packages=find_packages(where=PACKAGE_DIRS),
+    )
+
+
+if __name__ == "__main__":
+    main()
