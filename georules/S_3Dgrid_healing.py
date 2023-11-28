ec8db61fac4b00bdb985dc7d8caee965020c881d
dwillhelm <dwillhelm@enthought.com>
2023-11-28 15:15:49 -0600

    REF: more extraction

--- georules/S_3Dgrid_healing.py
+++ georules/S_3Dgrid_healing.py
@@ -85,6 +85,85 @@ def get_cell_num(n, cellsize_z, mud_property, nx, ny, Bathymetry_maps, sandbox_g
                             cell_num[j,i] = 0
     return cell_num
 
+def get_initial_sandbox(sandbox_grid, cellsize_z, lobe_tmax, nx, ny, grid_lobe_normal_mud, new_coord, i, x_c, y_c) -> np.ndarray:
+    """Update sandbox grid."""
+    x_nc_ur = round(new_coord[i][0])
+    y_nc_ur = round(new_coord[i][1])
+                    
+    x_nc_udr = math.floor(new_coord[i][0])
+    y_nc_udr = math.floor(new_coord[i][1])
+                
+                    
+    if verify_range(x_nc_ur, y_nc_ur, x_nc_udr, y_nc_udr, nx, ny): #### esto se puede cambiar!!!!!
+        for k in range(int(lobe_tmax/cellsize_z)):
+            if sandbox_grid[y_nc_ur,x_nc_ur,k] == 0:
+                sandbox_grid[y_nc_ur,x_nc_ur,k] =  grid_lobe_normal_mud[x_c,y_c,k]
+                              
+            if sandbox_grid[y_nc_udr,x_nc_udr,k] == 0:
+                sandbox_grid[y_nc_udr,x_nc_udr,k] =  grid_lobe_normal_mud[x_c,y_c,k]
+                              
+            if sandbox_grid[y_nc_udr,x_nc_ur,k] == 0:
+                sandbox_grid[y_nc_udr,x_nc_ur,k] = grid_lobe_normal_mud[x_c,y_c,k]
+                              
+            if sandbox_grid[y_nc_ur,x_nc_udr,k] == 0:
+               sandbox_grid[y_nc_ur,x_nc_udr,k] = grid_lobe_normal_mud[x_c,y_c,k]
+    return sandbox_grid
+
+def intialize_sandbox_grid(lobe_image, cellsize_z, lobe_tmax, nx, ny, nz, angle_stack, columns_corner, rows_corner, coord, grid_lobe_normal_mud, n):
+    sandbox_grid = np.zeros((nx,ny,int(nz/cellsize_z)))
+    new_coord = rotate_coord(
+                coord=coord,
+                angle=angle_stack[n-1],
+                image=lobe_image,
+                column_corner=columns_corner[n-1],
+                row_corner=rows_corner[n-1], 
+            )
+    for i in range(len(coord)):
+        x_c = coord[i][0]
+        y_c = coord[i][1]
+        sandbox_grid = get_initial_sandbox(sandbox_grid, cellsize_z, lobe_tmax, nx, ny, grid_lobe_normal_mud, new_coord, i, x_c, y_c)
+    return sandbox_grid
+
+def get_new_lobe(lobe_length, lobe_wmax, cell_size, cellsize_z, global_prop, mud_property, n_cell_mud, a1, a2, Bathymetry_maps, n):
+    bath_map = Bathymetry_maps[n]
+    bath_map_anterior = Bathymetry_maps[n-1] 
+    bath_dif = bath_map - bath_map_anterior 
+    bath_max = np.max(bath_dif)
+
+                ### Find new lobe with new thickness 
+    lobe_thickness_new = drop_geometry(
+                    wmax=lobe_wmax,
+                    lenght=lobe_length,
+                    tmax=bath_max, 
+                    x=[i for i in range(0, lobe_length, 100)],
+                    y_interval=cell_size,
+                    a1=a1,
+                    a2=a2,
+                )
+    grid_lobe_new =  grid_lobe(
+                    lobe_thi=lobe_thickness_new,
+                    length=lobe_length,
+                    wmax=lobe_wmax,
+                    cellsize_z=cellsize_z,
+                    cell_size=cell_size,
+                    tmax=bath_max,
+                    global_property=global_prop,
+                    mud_prop=mud_property,
+                    n_cell_mud=n_cell_mud,
+                    mud_layer=0,
+                    a1=a1,
+                    a2=a2, 
+                )
+    
+    return bath_dif,lobe_thickness_new,grid_lobe_new
+
+def get_verified_coords(nx, ny, coord, new_coord):
+    verify_range_index = np.where((new_coord[:, 0] >= 0) & (new_coord[:, 0] <= nx-1) & (new_coord[:, 1] >= 0) & (new_coord[:, 1] <= ny-1))[0] 
+                #New coordinates of the values inside the sandbox
+    verify_range_new_coord = new_coord[verify_range_index]
+                #Find the equivalent coordinates of the verify_range_new_coord using the indices 
+    verify_range_coord = coord[verify_range_index]
+    return verify_range_new_coord,verify_range_coord
 
 def sandbox(lobe_length,lobe_wmax,cell_size,lobe_image,cellsize_z,lobe_tmax,
             global_prop,mud_property,n_cell_mud,a1,a2,nx,ny,nz,n_lobe,angle_stack,
@@ -140,10 +219,7 @@ def sandbox(lobe_length,lobe_wmax,cell_size,lobe_image,cellsize_z,lobe_tmax,
    sandboxgrid : array
        3D grid of lobes with facies.
    """
-    
-    #Built standard lobe
-    lobe_x =  [i for i in range(0,lobe_length,100)]
-    
+
     #Find coordinates of standard lobe in grid system
     coord = get_standard_lobe_coordinates(lobe_length, lobe_wmax, cell_size)
     
@@ -163,99 +239,34 @@ def sandbox(lobe_length,lobe_wmax,cell_size,lobe_image,cellsize_z,lobe_tmax,
         a2=a2,
     )
     
-    #Sandbox_grid
-    sandbox_grid = np.zeros((nx,ny,int(nz/cellsize_z)))
-    
-    #nz_values - values for z axis
-    nz_values = np.linspace(0,nz-cellsize_z,int(nz/cellsize_z))
-    
     for n in range(1,n_lobe+1): 
-        
         if n == 1:
             print(n)
-            
-            new_coord = rotate_coord(
-                coord=coord,
-                angle=angle_stack[n-1],
-                image=lobe_image,
-                column_corner=columns_corner[n-1],
-                row_corner=rows_corner[n-1], 
-            )
-            
-            for i in range(len(coord)):
-                x_c = coord[i][0]
-                y_c = coord[i][1]
-                    
-                x_nc_ur = round(new_coord[i][0])
-                y_nc_ur = round(new_coord[i][1])
-                    
-                x_nc_udr = math.floor(new_coord[i][0])
-                y_nc_udr = math.floor(new_coord[i][1])
-                
-                    
-                if verify_range(x_nc_ur, y_nc_ur, x_nc_udr, y_nc_udr, nx, ny): #### esto se puede cambiar!!!!!
-                    
-                    for k in range(int(lobe_tmax/cellsize_z)):
-                        if sandbox_grid[y_nc_ur,x_nc_ur,k] == 0:
-                            sandbox_grid[y_nc_ur,x_nc_ur,k] =  grid_lobe_normal_mud[x_c,y_c,k]
-                              
-                        if sandbox_grid[y_nc_udr,x_nc_udr,k] == 0:
-                            sandbox_grid[y_nc_udr,x_nc_udr,k] =  grid_lobe_normal_mud[x_c,y_c,k]
-                              
-                        if sandbox_grid[y_nc_udr,x_nc_ur,k] == 0:
-                            sandbox_grid[y_nc_udr,x_nc_ur,k] = grid_lobe_normal_mud[x_c,y_c,k]
-                              
-                        if sandbox_grid[y_nc_ur,x_nc_udr,k] == 0:
-                           sandbox_grid[y_nc_ur,x_nc_udr,k] = grid_lobe_normal_mud[x_c,y_c,k] 
-                       
+            # intialize sandbox grid
+            sandbox_grid = intialize_sandbox_grid(lobe_image, cellsize_z, lobe_tmax, nx, ny, nz, angle_stack, columns_corner, rows_corner, coord, grid_lobe_normal_mud, n)
+
         else:
             
             if quadrants[n-1] == "HF":
                 print(n)
+                #nz_values - values for z axis
+                nz_values = np.linspace(0, nz-cellsize_z, int(nz/cellsize_z))
                 cell_num = get_cell_num(n, cellsize_z, mud_property, nx, ny, Bathymetry_maps, sandbox_grid, nz_values)
+                
+                # NOTE: It looks like you do not use `cell_num` any where in the code after 
+                # you create it - why do you have it? 
         
             else: 
                 print(n)
                 
-                bath_map = Bathymetry_maps[n]
-                bath_map_anterior = Bathymetry_maps[n-1] 
-                bath_dif = bath_map - bath_map_anterior 
-                bath_max = np.max(bath_dif)
-
-                ### Find new lobe with new thickness 
-                lobe_thickness_new = drop_geometry(
-                    wmax=lobe_wmax,
-                    lenght=lobe_length,
-                    tmax=bath_max, 
-                    x=lobe_x,
-                    y_interval=cell_size,
-                    a1=a1,
-                    a2=a2,
-                )
-                grid_lobe_new =  grid_lobe(
-                    lobe_thi=lobe_thickness_new,
-                    length=lobe_length,
-                    wmax=lobe_wmax,
-                    cellsize_z=cellsize_z,
-                    cell_size=cell_size,
-                    tmax=bath_max,
-                    global_property=global_prop,
-                    mud_prop=mud_property,
-                    n_cell_mud=n_cell_mud,
-                    mud_layer=0,
-                    a1=a1,
-                    a2=a2, 
-                )  
+                bath_dif, lobe_thickness_new, grid_lobe_new = get_new_lobe(lobe_length, lobe_wmax, cell_size, cellsize_z, global_prop, mud_property, n_cell_mud, a1, a2, Bathymetry_maps, n)  
 
                 #Find new coordinates - new_coord is equivalent to coord
-                new_coord = rotate_coord(coord,angle_stack[n-1],lobe_image,columns_corner[n-1],rows_corner[n-1])
+                new_coord = rotate_coord(coord, angle_stack[n-1], lobe_image, columns_corner[n-1], rows_corner[n-1])
+                
                 #Filter values that are outside the sandbox
                 #Index of the values inside the sandbox
-                verify_range_index = np.where((new_coord[:, 0] >= 0) & (new_coord[:, 0] <= nx-1) & (new_coord[:, 1] >= 0) & (new_coord[:, 1] <= ny-1))[0] 
-                #New coordinates of the values inside the sandbox
-                verify_range_new_coord = new_coord[verify_range_index]
-                #Find the equivalent coordinates of the verify_range_new_coord using the indices 
-                verify_range_coord = coord[verify_range_index]
+                verify_range_new_coord, verify_range_coord = get_verified_coords(nx, ny, coord, new_coord)
                 
                 ###create array to verify which coordinates have been filled out
                 fill_out_array = np.zeros((nx,ny))
@@ -275,7 +286,7 @@ def sandbox(lobe_length,lobe_wmax,cell_size,lobe_image,cellsize_z,lobe_tmax,
                      if bath_dif[y_nc_ceil][x_nc_ceil]> 0.0001 and fill_out_array[y_nc_ceil][x_nc_ceil] == 0:
                          fill_out_array[y_nc_ceil][x_nc_ceil] = 1
                          
-                         z_cell_number = round(lobe_thickness_new[y_c,x_c]/cellsize_z) 
+                         z_cell_number = round(lobe_thickness_new[y_c, x_c]/cellsize_z) 
                 
                         ### Find where the deposition starts
                          j = np.count_nonzero(sandbox_grid[y_nc_ceil,x_nc_ceil,:])
@@ -338,3 +349,10 @@ def sandbox(lobe_length,lobe_wmax,cell_size,lobe_image,cellsize_z,lobe_tmax,
                    
      
     return(sandbox_grid)
+
+
+
+
+
+
+
