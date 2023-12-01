0b17c56e0f0ad417d01686925b02920c6680070d
Chacon Buitrago <nchaconb@my.utexas.edu>
2023-11-30 22:54:40 -0600

    added functions to save list as json and load  list as json.

--- georules/utils.py
+++ georules/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import List
 import numpy as np 
+import json
 
 def save_bath_as_array(filename:Path, bath_map:List[np.ndarray]) -> None:
     """Serialize a list of bathymetry arrays into a 3D array.""" 
@@ -13,6 +14,19 @@ def load_bath_binary(filename:Path) -> np.ndarray:
     with open(filename, 'rb') as fh: 
         res = np.load(filename)
     return res
+
+
+def save_list_as_json(filename:Path,_list) -> None:
+    """Saves a list like a json file"""
+    with open(filename, "w") as fh:
+        json.dump(filename,fh)
+
+def load_list_json(filename:Path) -> list:
+    """ Load list from json file"""
+    with open(filename, "r") as file:
+        loaded_list = json.load(file)
+        return(loaded_list)
+
 
 if __name__ == '__main__':
     # test case 
