7eedc6ea62db7320ac4a918edc5c51d7e38d0d9a
Chacon Buitrago <nchaconb@my.utexas.edu>
2023-11-30 22:39:43 -0600

    'variance_bathymetry_maps' was added to 'bathymetry.py,' and 'arr' was changed to '_map'.

--- georules/bathymetry.py
+++ georules/bathymetry.py
@@ -27,3 +27,37 @@ class BathymetryLayers:
         ax.set_title(f"layer-{idx}") 
         ax.imshow(self.layers[idx])
         return fig, ax
+
+def varinace_bathymetry_maps(Bathymetry_maps):
+    """Calculate Variance of thicknesses in bathymetry maps
+       
+       Parameters
+       ----------  
+       Bathymetry_maps : List
+       Bathymetry layer instance.
+       
+       Returns
+       -------
+       variance_bathymetry: arrays
+       array with the variance of the thicknesses of each bathymetry map. 
+            
+
+    """ 
+
+
+    variance_bathymetry = []
+    
+    for _map in Bathymetry_maps:
+        
+        # calculate mean
+        mean_array = np.mean(_map)
+        # calculate squared differences
+        squared_diff = (_map - mean_array)**2
+        #calculate variance
+        variance_array = np.mean(squared_diff)
+        #append variance to the bathymetry map
+        variance_bathymetry.append(variance_array)
+        
+    # Convert the list to a NumPy array
+    
+    return(np.array(variance_bathymetry))
