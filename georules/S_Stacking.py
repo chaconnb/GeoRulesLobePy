1772f8f31cf541df025a51ec1d536272d252136b
chaconnb <108029531+chaconnb@users.noreply.github.com>
2023-12-26 16:56:58 -0500

    S_Stacking.py: Docstrings and small changes.

--- georules/S_Stacking.py
+++ georules/S_Stacking.py
@@ -2,40 +2,43 @@
 """
 Created on Sun Mar 19 11:16:23 2023
 
-@author: Nataly Chacon-Buitrag
-                                                                                                                 
-Function to stack n+1 lobe depending on the position of lobe n.                                                  0/360
-Input:                                                                                                            |
-angle_move1 and angle_move_2 : give the range of movement of the lobe whith respect to those two angles. 90 ----- | ----- 270
-                              The range of movement will go from angle_move1 to angle_move2                       |
-                                                                                                                 180                                                                                                              
-
+@author: Nataly Chacon-Buitrago
+                                                                                                                                                                                                                                                                                                                                                                    
 """
 import numpy as np
-import matplotlib.pyplot as plt 
 
 
-def stacking(
-        centroid, 
-        n,
-        lobe_radius,
-        nx,
-        ny,
-        bathymetry_layer,
-        angle_move1,
-        angle_move2,
-    ):
+def stacking(centroid, n, lobe_radius, nx, ny, bathymetry_layer, angle_move1, angle_move2):
     
+    """Function to stack n+1 lobe depending on the position of lobe n. 
+       The stacking pattern depends on the state of the markov chain. 
+       
+    Parameters
+    ----------
+    centroid: list 
+        Centroid's coordiantes, in the format [column,row].
+    n : int
+        Number of lobe being deposited.
+    lobe_radius: float
+    nx : int
+        Sandbox size in the x direction.
+    ny : int
+        Sandbox size in the y direction.
+    bathymetry_layer: np.array
+        Bathymetry layer generated from n-1 markov state.
+    angle_move1 and angle_move_2: int    
+        Range of movement of the centroid is given by this two angles.
+        The range of movement will go from angle_move1 to angle_move2.
     
-    ###check : plot bathymetry layer and centroid
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title(n)
-    plt.imshow(bathymetry_layer)
-    plt.scatter(centroid[0],centroid[1])
-    plt.colorbar()
-    plt.show()
-    ####
+    Returns
+    -------
+    lobe_location:list
+        Centroid's coordiantes for next lobe.
+    prob_s_b: np.array 
+        Probability map for finding new centroid. Topographic highs have low 
+        probabilities and topographic lows have high probabilities. 
+    
+    """
     
     
     rad_int = lobe_radius + lobe_radius * 0.00002
@@ -67,22 +70,9 @@ def stacking(
     if moving_mask[a,b] != 1.0: 
         raise ValueError('Lobe coordinates are not in the correct quadrant.')
 
-    lobe_location = [b,a] #location of the centroid
-
+    lobe_location = [b,a] #location of the centroid, I had to swap the positions of 'a' and 'b' to make it work.
     
-    ###check : plot mask with probabilities
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title("angle 1: {}  angle 2: {}".format(angle_move1, angle_move2))
-    plt.imshow(norm_prob_s)
-    plt.scatter(centroid[0],centroid[1])
-    plt.scatter(b,a)
-    plt.colorbar()
-    plt.show()
-    ####
-    
-    
-    return(lobe_location, prob_s, prob_s_b)
+    return(lobe_location, prob_s_b)
 
 def get_moving_mask(nx, ny, angle_move1, angle_move2, centroid, rad_int):
     
@@ -92,22 +82,42 @@ def get_moving_mask(nx, ny, angle_move1, angle_move2, centroid, rad_int):
         
     #angle mask
     #1 if it is inside angle_move1 and angle_move2:
-    prob_mask = get_prob_mask(nx, ny, angle_move1, angle_move2, centroid_angles)
+    angle_mask = get_angle_mask(nx, ny, angle_move1, angle_move2, centroid_angles)
                         
     # # overlap circle mask  ang angle mask
-    result = get_overlap(nx, ny, circle_mask, prob_mask)
+    result = get_overlap_mask(nx, ny, circle_mask, angle_mask)
     return result
 
-def get_overlap(nx, ny, circle_mask, prob_mask):
+def get_circle_mask(nx, ny, centroid, rad_int):
+    "Mask from the centroid to the radius (rad_int) in every direction."
+    
     mask = np.zeros((nx,ny))
 
     for i in range(0,nx):
         for j in range(0,ny):
-            if prob_mask[i][j] ==circle_mask[i][j] and prob_mask[i][j] == 1 and circle_mask[i][j] == 1:
-                mask[i][j] = 1
+            dis_centroid = (j - centroid[0])**2 + (i-centroid[1])**2# distance from the point(x,y) to the centroid
+                 
+            if dis_centroid< rad_int**2:
+                mask[i,j]= 1
     return mask
 
-def get_prob_mask(nx, ny, angle_move1, angle_move2, centroid_angles):
+def get_centroid_angles(nx, ny, centroid):
+    "Finds the angle in degrees from the centroid to every cell in the grid of size nx * ny."
+    
+    angle_rad = np.zeros((nx,ny))
+
+    for i in range(0,nx):
+        for j in range(0,ny):
+                angle_rad[i,j]= np.arctan2((i-centroid[1]),(j-centroid[0])) #find the angle from the centroid -radians-
+                
+    angle_deg = np.rad2deg(angle_rad) #Find the angle from the centroid -degrees-
+    angle_deg = angle_deg + 180 # Find the angle from  the centroid from 0 - 360
+    angle_deg[angle_deg == 360] = 0
+    return angle_deg
+
+def get_angle_mask(nx, ny, angle_move1, angle_move2, centroid_angles):
+    "Finds mask between the angle1 and angle 2"
+    
     mask = np.zeros((nx,ny)) #mask to determine where the lobe is going to move based on the angles
 
     for i in range(0,nx):
@@ -123,28 +133,20 @@ def get_prob_mask(nx, ny, angle_move1, angle_move2, centroid_angles):
                     mask[i][j] = 1
     return mask
 
-def get_centroid_angles(nx, ny, centroid):
-    angle_rad = np.zeros((nx,ny))
 
-    for i in range(0,nx):
-        for j in range(0,ny):
-                angle_rad[i,j]= np.arctan2((i-centroid[1]),(j-centroid[0])) #find the angle from the centroid -radians-
-                
-    angle_deg = np.rad2deg(angle_rad) #Find the angle from the centroid -degrees-
-    angle_deg = angle_deg + 180 # Find the angle from  the centroid from 0 - 360
-    angle_deg[angle_deg == 360] = 0
-    return angle_deg
-
-def get_circle_mask(nx, ny, centroid, rad_int):
+def get_overlap_mask(nx, ny, circle_mask, angle_mask):
+    "Overlaps angle_mask and circle_mask to create a mask between the angles and length from the centroid of interest."
     mask = np.zeros((nx,ny))
 
     for i in range(0,nx):
         for j in range(0,ny):
-            dis_centroid = (j - centroid[0])**2 + (i-centroid[1])**2# distance from the point(x,y) to the centroid
-                 
-            if dis_centroid< rad_int**2:
-                mask[i,j]= 1
+            if angle_mask[i][j] ==circle_mask[i][j] and angle_mask[i][j] == 1 and angle_mask[i][j] == 1:
+                mask[i][j] = 1
     return mask
-    
-            
-                
+
+
+
+
+
+
+                            
