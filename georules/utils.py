9af3ede8dfae59964f4a0912c6614fc53d4e2f40
Nataly Chacon-Buitrago <108029531+chaconnb@users.noreply.github.com>
2024-01-12 09:39:32 -0600

    Run_Simulations (#47)
    * small change
    
    * added more function to the utils.py file
    
    * M_LRBM_test.py: Outputs go to different folders.
    
    * added numpy
    
    * test

--- georules/utils.py
+++ georules/utils.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 from typing import List
 import numpy as np 
+import math
 import json
-import os
+
 
 def save_bath_as_array(filename:Path, foldername:Path, bath_map:List[np.ndarray]) -> None:
     """Serialize a list of bathymetry arrays into a 3D array.""" 
     _array = np.stack(bath_map)
-    #file_path = np.save(os.path.join(foldername,filename), _array)
     file_path = Path(foldername) /Path(filename)  
     with open(file_path, 'wb') as fh:  # Open in binary mode ('wb')
         np.save(fh, _array)
@@ -18,11 +18,22 @@ def save_array(filename:Path, foldername:Path,_array:np.ndarray) -> None:
     file_path = Path(foldername) /Path(filename) 
     with open(file_path,'wb') as fh:
         np.save(fh, _array)
+        
+def save_centroids(filename:Path, foldername:Path,_list) -> None:
+    """Saves list in .npy file.""" 
+    for i in range(len(_list)):
+         if not _list[i]:
+             _list[i] = [math.nan, math.nan]       
+    _array = np.stack(_list)          
+    file_path = Path(foldername)/Path(filename) 
+    with open(file_path,'wb') as fh:
+        np.save(fh, _array)
 
-def load_bath_binary(filename:Path) -> np.ndarray:
-    """Load a serialized bathymetry."""
-    with open(filename, 'rb') as fh: 
-        res = np.load(filename)
+def load_array(filename:Path, foldername:Path) -> np.ndarray:
+    """Load a serialized array."""
+    file_path = Path(foldername)/Path(filename)
+    with open(file_path, 'rb') as fh: 
+        res = np.load(fh)
     return res
 
 def save_list_as_json(filename:Path, foldername:Path, _list) -> None:
@@ -33,9 +44,11 @@ def save_list_as_json(filename:Path, foldername:Path, _list) -> None:
         json.dump(_list, fh)
 
 
-def load_list_json(filename:Path) -> list:
+def load_list_json(filename:Path, foldername:Path) -> list:
     """ Load list from json file"""
-    with open(filename, "r") as file:
+    folder_path = Path(foldername)
+    file_path = folder_path / (filename +".json")
+    with open(file_path, "r") as file:
         loaded_list = json.load(file)
         return(loaded_list)
 
@@ -46,11 +59,7 @@ if __name__ == '__main__':
     for _ in range(10): 
         l.append(np.zeros(shape=(5,5))) 
 
-    test_file = Path("../test_array.npy")
-    save_bath_as_array(test_file, l)
+    save_bath_as_array("test_file.npy","results",l)
 
-    bla = load_bath_binary(test_file)
+    bla = load_array("test_file.npy","results") #results is the folder name
     print(f"Arrays Equal: {np.array_equal(l, bla)}") 
-
-    # cleanup
-    test_file.unlink() 
